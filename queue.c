c7dde3cec9dec45e30f30139c24bf8c45652e4e3
Abhisek Phago limbu <112129568+AbhisekLimbu@users.noreply.github.com>
2025-11-10 17:58:17 -0600

    something is fishy

--- queue.c
+++ queue.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define LIMIT 5	
 
 typedef struct{
 	int* head;
@@ -13,6 +14,10 @@ typedef struct{
 void init_queue(queue_t* x, size_t cap_t){
 	x -> size = cap_t;
 	x->d_block = malloc(sizeof(int) * cap_t);
+	if(!x->d_block){
+		printf("FAILED ALLOCATING MEMORY:\n");
+		exit(1);
+	}
 	x->head = x->d_block;
 	x->tail = x->d_block;
 	x->count = 0;
@@ -30,19 +35,34 @@ void enqueue_t(queue_t* x, int num)
 
 }
 
-// 
-//int dequeue_t(){
-//	
-//}
+void dequeue_t(queue_t* x){
+	if(x-> head == x->tail){
+		printf("two pointer at the same mem loc: or zero \n");
+		exit(1);
+	}
+	*(x->head) = 0;
+	x->head--;
+}
 
 
 int main(){
 	queue_t my_queue;
 	queue_t *ptr = &my_queue;
-	init_queue(ptr, 6);
-	enqueue_t(ptr, 10);
-	printf("%d\n", ptr->d_block[0]);
+	init_queue(ptr, LIMIT);
+	// enquining
+	for(int i = 0; i < LIMIT; i++){
+		enqueue_t(ptr, i);
+	}
+	//printing
+	//for(int i = 0; i < ptr->head; i++){
+	//	queue_t* iter_ptr = ptr->d_block;
+	//	printf("%d\n", *(itr_ptr(i));
+	//
+	//}
 
+
+	printf("%d\n", ptr->d_block[0]);
+	printf("%d\n", ptr->d_block[1]);
 	free(ptr->d_block);
 	return 0;
 }
